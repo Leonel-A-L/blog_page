d615570c840f00790a1781397070725a5e54b6c3
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-12 19:23:00 -0400

    webpage the post site

---
+++ client/src/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return(
+        <div className='entries'>
+            <div className='post'>
+            <div className='img'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ7JKmZpkjrZMA_iAyPTT3vhk4kA38aO78TA&usqp=CAU'alt='Camaro Pic'></img>
+            </div>
+            <div className='texts'>
+                <h2>Here's When 2024 Chevy Camaro Production Is Scheduled To End</h2>
+                <p className='info'>
+                <a className='author'>Leonel Lamelas</a>
+                <time>2023-09-11 10:15</time>
+                </p>
+                <p className='summary'>The end of production for the 2024 Chevy Camaro is an important date and milestone in the sports car's history, so it's worth recalling when the last units will roll off the assembly line at GM factories.</p>
+            </div>
+            </div>
+        </div>
+    )
+}

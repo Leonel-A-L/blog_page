7e1be08abd6c0331216b21315844cdd1bbbacd08
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-13 17:49:03 -0400

    placed the usercontext to the app

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,22 @@ import Layout from './Layout/Layout';
 import IndexPage from './Pages/IndexPage';
 import LoginPage from './Pages/LoginPage';
 import RegisterPage from './Pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
+import CreatePost from './Pages/CreatePost';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-          <Route  index element={<IndexPage /> }/>
-          <Route path='/login' element={<LoginPage />}/>
-          <Route path='/register' element={<RegisterPage />}/>
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+            <Route  index element={<IndexPage /> }/>
+            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/register' element={<RegisterPage />}/>
+            <Route path='/createPost' element={<CreatePost />}/>
+
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 

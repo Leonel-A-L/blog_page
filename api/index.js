ed48b16689ef3000e04b8eeb3d7975aa972a0d3f
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-13 11:38:04 -0400

    added JWT and Coockie for authentication and a new route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+const salt = bcrypt.genSaltSync(10);
 
 
 
@@ -9,13 +14,16 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cookieParser())
 
 const createUsers = require("./routes/Register");
+const userLogin = require("./routes/Login")
 
 
 //routes
 app.use('/register', createUsers)
+app.use('/login', userLogin)
 
 // db connection
 mongoose

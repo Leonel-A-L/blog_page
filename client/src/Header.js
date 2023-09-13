4b19790d4c6d84cc9c7e153c3755d3e29c8440ce
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-13 12:45:21 -0400

    addjust header deppending if user logged in and working on bugs

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,41 +1,63 @@
-import {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
-export default function Header(){
-    const [username, setUsername] = useState(null)
-    useEffect(() => {
-        fetch('http://localhost:8080/login/profile',{
-            credentials:'include'
-        }).then(response => {
-            response.json().then(userInfo => {
-                setUsername(userInfo.username);
-            })
-        })
-    },[])
+export default function Header() {
+  const [username, setUsername] = useState(null);
 
-    function logout(){
-        fetch('http://localhost:8080/login/logout',{
-            credentials: 'include',
-            method: 'POST'
-        })
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/login/profile', {
+          credentials: 'include',
+        });
+        if (response.ok) {
+          const userInfo = await response.json();
+          setUsername(userInfo.username);
+        } else {
+          console.error('Failed to fetch user profile');
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/login/logout', {
+        credentials: 'include',
+        method: 'POST',
+      });
+
+      if (response.ok) {
+        console.log('Logout successful');
+      } else {
+        console.error('Failed to logout');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
     }
+  };
 
-    return(
-        <header>
-            <a href='/' className='logo'>MyBlog</a>
-            <nav>
-                {username && (
-                    <>  
-                        <a href='/create'>Create new post</a>    
-                        <a onClick={logout}>Logout</a>                    
-                    </>
-                )}
-                {!username && (
-                    <>
-                         <a href='/login'>Login</a>
-                        <a href='/register'>Register</a>
-                    </>
-                )}
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <a href='/' className='logo'>
+        MyBlog
+      </a>
+      <nav>
+        {username ? (
+          <>
+            <a href='/create'>Create new post</a>
+            <a onClick={handleLogout}>Logout</a>
+          </>
+        ) : (
+          <>
+            <a href='/login'>Login</a>
+            <a href='/register'>Register</a>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }

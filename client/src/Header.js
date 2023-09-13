89f363df9462e631315dcde46787e5e0821c967b
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-13 11:40:12 -0400

    added authentication to the headers to change depending if logged in

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,10 +1,40 @@
+import {useEffect, useState} from 'react'
+
 export default function Header(){
+    const [username, setUsername] = useState(null)
+    useEffect(() => {
+        fetch('http://localhost:8080/login/profile',{
+            credentials:'include'
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username);
+            })
+        })
+    },[])
+
+    function logout(){
+        fetch('http://localhost:8080/login/logout',{
+            credentials: 'include',
+            method: 'POST'
+        })
+    }
+
     return(
         <header>
             <a href='/' className='logo'>MyBlog</a>
             <nav>
-            <a href='/login'>Login</a>
-            <a href='/register'>Register</a>
+                {username && (
+                    <>  
+                        <a href='/create'>Create new post</a>    
+                        <a onClick={logout}>Logout</a>                    
+                    </>
+                )}
+                {!username && (
+                    <>
+                         <a href='/login'>Login</a>
+                        <a href='/register'>Register</a>
+                    </>
+                )}
             </nav>
         </header>
     )

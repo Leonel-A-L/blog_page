aede8b6602bd39ad721e9a70598fdd114c239671
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-14 21:30:45 -0400

    created the create post section added some quill for the blog

--- frontend/src/Pages/CreatePost.js
+++ frontend/src/Pages/CreatePost.js
@@ -1,10 +1,69 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill'; 
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+  toolbar: [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],
+    [{ 'indent': '-1' }, { 'indent': '+1' }],
+    [{ 'direction': 'rtl' }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'color': [] }, { 'background': [] }],
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header', 'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent', 'link', 'image'
+];
+
 export default function CreatePost() {
-    return (
-         <form>
-            <input type="title" placeholder="{Title}" />
-            <input type="summary" placeholder="{Summary}" />
-            <input type="file" />
-            <textarea name='' id="" cols='30' rows='10'></textarea>
-         </form>
-    )
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [file, setFile] = useState(null); // Initialize file state as null
+
+  function createNewPost(ev) {
+    ev.preventDefault();
+
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.append('file', file); // Use append to add files to FormData
+
+    console.log(data);
+
+    fetch('http://localhost:8080/post', {
+      method: 'POST',
+      body: data,
+    })
+      .then(response => response.json())
+      .then(result => {
+        // Handle the response/result from the server
+        console.log(result);
+      })
+      .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input type="text" placeholder="Title" value={title} onChange={(ev) => setTitle(ev.target.value)} />
+      <input type="text" placeholder="Summary" value={summary} onChange={(ev) => setSummary(ev.target.value)} />
+      <input type="file" onChange={(ev) => setFile(ev.target.files[0])} /> {/* Use ev.target.files */}
+      <ReactQuill value={content} modules={modules} formats={formats} onChange={newValue => setContent(newValue)} />
+      <button style={{ marginTop: '5px' }} type="submit">Create post</button>
+    </form>
+  );
 }

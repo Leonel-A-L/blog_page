610c29c3b7017ca776e21a408278a64c14686e1b
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-16 10:50:10 -0400

    added the post routes

--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,1 +1,13 @@
 const router = require("express").Router();
+const { createPost, getPosts, getPostById } = require("../controllers/Posts");
+
+//Create Post 
+router.post("/",  createPost)
+
+//Get Post
+router.get("/", getPosts)
+
+//Get Post By Id
+router.get("/:id", getPostById)
+
+module.exports = router

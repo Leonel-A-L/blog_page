8ae8e620956fef571df997ef4d2b45de5af6cfe3
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-17 10:51:27 -0400

    deleted un-necessary routes and added a new one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import { Route, Router, Routes } from 'react-router';
 import './App.css';
-import Header from './Header';
-import Post from './Post';
 import Layout from './Layout/Layout';
 import IndexPage from './Pages/IndexPage';
 import LoginPage from './Pages/LoginPage';
@@ -9,6 +7,7 @@ import RegisterPage from './Pages/RegisterPage';
 import { UserContextProvider } from './UserContext';
 import CreatePost from './Pages/CreatePost';
 import SelectedPost from './Pages/SelectedPost';
+import EditPost from './Pages/EditPost';
 
 function App() {
   return (
@@ -20,6 +19,7 @@ function App() {
             <Route path='/register' element={<RegisterPage />}/>
             <Route path='/createPost' element={<CreatePost />}/>
             <Route path='/post/:id' element={<SelectedPost />}/>
+            <Route path='/post/edit/:id' element={<EditPost />}/>
         </Route>
       </Routes>
     </UserContextProvider>

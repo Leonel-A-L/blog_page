57b0f2b4130172ef825f54ad7996ed5386f590ad
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-19 20:23:11 -0400

    added a delete function to the backend and fixed some bugs

--- frontend/src/Pages/SelectedPost.js
+++ frontend/src/Pages/SelectedPost.js
@@ -1,43 +1,85 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router"
-import React, { useContext } from 'react';
-import { format } from "date-fns"
-import { UserContext } from "../UserContext";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, Link, Navigate } from 'react-router-dom';
+import { format } from 'date-fns';
+import { UserContext } from '../UserContext';
 
 export default function SelectedPost() {
-    const {id} = useParams();
-    const [postInfo, setPostInfo] = useState(null)
-    const {userInfo} = useContext(UserContext)
-    useEffect(() => {
-        fetch(`http://localhost:8080/post/${id}`).then(response => {
-            response.json().then(postInfo => {
-                setPostInfo(postInfo)
-            })
-        })
-    },[])
-    if (!postInfo) return'';
-    return (
-        <div className="selectedPost">
-            <h1>{postInfo.title}</h1>
-            <time className="time">{format(new Date(postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
-            <div className="author">
-                by @{postInfo.author.username}                
-            </div>
-            {userInfo.id === postInfo.author._id && (
-                <div className="edit-row">
-                    <Link className="editPostButton" to={`/post/edit/${postInfo._id}`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+  const { id } = useParams();
+  const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/post/${id}`)
+      .then((response) => {
+        response.json().then((postInfo) => {
+          setPostInfo(postInfo);
+        });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [id]);
+
+  async function deletePost() {
+    try {
+      const response = await fetch(`http://localhost:8080/post/${id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        setRedirect(true);
+        // Handle the response/result from the server
+        console.log('Post deleted successfully');
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      // Handle any errors that occurred during the fetch
+      console.error('Error:', error);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />;
+  }
+
+  if (!postInfo) return '';
+
+  return (
+    <div className="selectedPost">
+      <h1>{postInfo.title}</h1>
+      <time className="time">{format(new Date(postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
+      <div className="author">
+        by @{postInfo.author.username}
+      </div>
+      {userInfo.id === postInfo.author._id && (
+        <div className="edit-row">
+            <div className='authorChoiceBtn'>
+                <Link className="editPostButton" to={`/post/edit/${postInfo._id}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                     </svg>
-                        Edit Post
-                    </Link>
-                </div>
-            )}
-            <div className="img">
-                <img src={`http://localhost:8080/${postInfo.cover}`} alt="" />
+                    Edit Post
+                </Link>
             </div>
-            <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
         </div>
-    )
+      )}
+      <div className="img">
+        <img src={`http://localhost:8080/${postInfo.cover}`} alt="" />
+      </div>
+      <div className="content" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+      <div className='edit-row'>
+            <button className="deletePostButton" onClick={deletePost}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>
+                    Delete Post
+            </button>
+         </div>
+    </div>
+  );
 }
+
+
